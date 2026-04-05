8092017e839da3d052a40a18a25eddee5177f12a
ahmadev017 <ahmadraza01708@gmail.com>
2026-04-05 17:09:31 +0500

    intial commit3

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,17 +6,11 @@ const useCdnEnv = process.env.NEXT_PUBLIC_SANITY_USE_CDN;
 const useCdn =
   useCdnEnv !== undefined ? useCdnEnv !== "false" : process.env.NODE_ENV === "production";
 
-const timeoutEnv =
-  process.env.SANITY_REQUEST_TIMEOUT_MS ||
-  process.env.NEXT_PUBLIC_SANITY_REQUEST_TIMEOUT_MS;
-const requestTimeout = Number(timeoutEnv) || 5000;
-
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn,
-  requestTimeout,
   stega: {
     studioUrl: process.env.NEXT_PUBLIC_SANITY_STUDIO_URL,
   },
